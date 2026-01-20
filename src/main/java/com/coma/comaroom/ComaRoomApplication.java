88b9dc45e1cfc736c6ae0bf3c00cbc65e4ea3fb9
choi-jin-wook <jinwook219@gmail.com>
2026-01-20 18:02:56 +0900

    first commit

---
+++ src/main/java/com/coma/comaroom/ComaRoomApplication.java
@@ -0,0 +1,13 @@
+package com.coma.comaroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComaRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComaRoomApplication.class, args);
+	}
+
+}
