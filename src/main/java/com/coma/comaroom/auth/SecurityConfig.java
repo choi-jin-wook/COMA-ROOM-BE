72c77a57df4f9a938634a4eb23e04d7817d7c2a7
choi-jin-wook <jinwook219@gmail.com>
2026-05-17 08:34:46 +0900

    fix cors 요청 도메인만 허용

--- src/main/java/com/coma/comaroom/auth/SecurityConfig.java
+++ src/main/java/com/coma/comaroom/auth/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(List.of("*"));
+        configuration.setAllowedOrigins(List.of("https://www.comaroom.site"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("Authorization"));
